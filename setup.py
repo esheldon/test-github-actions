9555782794a581cac868d3f68cf354190e560d82
Erin Sheldon <erin.sheldon@gmail.com>
2020-04-30 13:33:33 -0400

    initial commits

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_github_actions",
+    version="0.1.0",
+    packages=find_packages(),
+    author='Erin Sheldon',
+    author_email='erin.sheldon@gmail.com',
+    url='https://github.com/esheldon/test_github_actions',
+)
